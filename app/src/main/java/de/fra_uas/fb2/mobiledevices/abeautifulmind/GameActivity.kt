25c19268bff44027c9d918743f0da1dd1bd984e8
iburahema <ibu23s@hotmail.com>
2023-05-17 12:47:23 +0200

    ERSTE VERSUCH

--- app/src/main/java/de/fra_uas/fb2/mobiledevices/abeautifulmind/GameActivity.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/abeautifulmind/GameActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 
-class GameActivity : AppCompatActivity() {
+class  GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
